063826ae3fa51168f75a75d1430441dfda9551db
Daniel Lewis <daniel@cloudspace.com>
2011-10-17 07:10:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Famijobs::Application.config.secret_token = '6f96dc4997483838e539f28f582b1bc131bbc807b73a5856fc587bb2c395901da5ea14da00b5144ca8598f4e6fcea60d19b165d5c4c495f9b826a6d3515f9b74'
